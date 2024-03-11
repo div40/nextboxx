7aad4a79c58a2c3f40e5e4ebfd53a9327cd08cc6
div40 <vidh40@gmail.com>
2024-03-11 18:43:48 +0530

    linking files with orgId or userId

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,27 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import {
-  SignIn,
   SignInButton,
   SignOutButton,
   SignedIn,
   SignedOut,
+  useOrganization,
+  useUser,
 } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const organization = useOrganization();
+  const user = useUser();
+
+  let orgId: string | undefined = undefined;
+  if (organization.isLoaded && user.isLoaded) {
+    orgId = organization.organization?.id ?? user.user?.id;
+  }
+
   const createFile = useMutation(api.files.createFile);
-  const files = useQuery(api.files.getFiles);
+  const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignedIn>
@@ -32,8 +41,10 @@ export default function Home() {
 
       <Button
         onClick={() => {
+          if (!orgId) return;
           createFile({
             name: "filename",
+            orgId,
           });
         }}
       >
